b63693996d78e6637b4d4bbc2b173fd7f6e7c42a
Putranugroho14 <putranugroho61@gmail.com>
2025-11-08 10:22:49 +0700

    membuat data class

---
+++ app/src/main/java/com/example/tugas/view/ListPeserta.kt
@@ -0,0 +1,22 @@
+package com.example.tugas.view
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.tugas.R
+
+data class Peserta(
+    val namaLengkap: String,
+    val jenisKelamin: String,
+    val statusKerja: String,
+    val alamat: String
+)
+
